02c0460459dd79a7edf3b617f8867ba5134b4716
aleksandrapopova <avtikhomirova@gmail.com>
2024-04-06 12:14:55 -0700

    1. First test for API project

--- src/main/java/nasa/api/models/Category.java
+++ src/main/java/nasa/api/models/Category.java
@@ -1,6 +1,5 @@
 package nasa.api.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/nasa/api/requests/CheckedRequests.java
+++ src/main/java/nasa/api/requests/CheckedRequests.java
@@ -3,7 +3,6 @@ package nasa.api.requests;
 import io.restassured.specification.RequestSpecification;
 import lombok.Getter;
 import nasa.api.requests.checked.CheckedEvents;
-import nasa.api.requests.unchecked.UncheckedEvents;
 
 @Getter
 public class CheckedRequests {
